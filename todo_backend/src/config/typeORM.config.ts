37a530cd7209ea0e84ec215cc8a75eea7b0f365b
HussienSayed14 <hussiens399@gmail.com>
2023-07-11 16:16:04 +0300

    Docker file added

--- todo_backend/src/config/typeORM.config.ts
+++ todo_backend/src/config/typeORM.config.ts
@@ -1,13 +1,25 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
+// export const typeOrmConfig: TypeOrmModuleOptions =
+// {
+//     type: 'mysql',
+//       host: 'localhost',
+//       port: 3306,
+//       username: 'root',
+//       password: 'root',
+//       database: 'todo_app',
+//       entities: ['dist/**/*.entity{.ts,.js}'],
+//       synchronize: true
+// };
+
 export const typeOrmConfig: TypeOrmModuleOptions =
 {
     type: 'mysql',
-      host: 'localhost',
-      port: 3306,
+      host: 'containers-us-west-146.railway.app',
+      port: 7588,
       username: 'root',
-      password: 'root',
-      database: 'todo_app',
+      password: 'pUKdjLZ3v1tn7koN7Lm8',
+      database: 'railway',
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true
 };
